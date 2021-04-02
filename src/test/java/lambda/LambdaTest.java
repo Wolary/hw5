e1836096cfd6885689874b8082a0ce6a4cd4d324
Wolary <Ktrnjh19>
2021-04-02 14:47:54 +0300

    Test 2

--- src/test/java/lambda/LambdaTest.java
+++ src/test/java/lambda/LambdaTest.java
@@ -29,6 +29,7 @@ public class LambdaTest {
             open(url);
         });
 //
+        //
         //заходим в репозиторий
         step("Заходим в репозиторий", () -> {
             $(".header-search-input").click();
